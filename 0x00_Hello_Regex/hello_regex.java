7f822dfdc385c2b858cf63eb25c114a9e3bbd06e
KayongoYongo <sknsammie10@gmail.com>
2025-01-09 13:24:55 +0300

    Foo bar regex

---
+++ 0x00_Hello_Regex/hello_regex.java
@@ -0,0 +1,11 @@
+import java.util.regex.*;
+
+public class RegexExample {
+	public static void main(String[] args) {
+		// Sample string
+		String text = "I scream, you scream, we all scream for Ice Cream";
+
+		// Pattern to search for
+		String pattern = "scream";
+
+		
